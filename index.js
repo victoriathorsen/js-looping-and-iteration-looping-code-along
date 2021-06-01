b7b8fb54a918ef7f02e55f20563816f447f56882
Victoria <victoriathorsenn@gmail.com>
2021-06-01 01:29:32 -0400

    commit tree db8b2901b2992250dc547c6f8cb7713b7f4753cd

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names) {
+    let arrayNames = []
+    for (let i = 0; i < names.length; i++) {
+        arrayNames.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return arrayNames
+}
+
+function countDown(i) {
+    while (i > 0) {
+        console.log(i)
+        i-=1
+    }
+    console.log(i)
+}
+
